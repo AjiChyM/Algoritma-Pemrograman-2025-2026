539dfd8cb2e7b7f7d39979530f23cb785da44754
AjiChyM <ajicahyamustaqim01@gmail.com>
2025-10-13 09:44:10 +0700

    latihan c++
    menambahkan latihan coding c++

---
+++ c atau c++/coba1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <conio.h> 
+ 
+main() 
+{ 
+    int a = 3;
+    int b = 1;
+    char c[15] = "aji";
+ 
+     
+    puts("Nama "); 
+    puts(c); 
+    printf("Terdiri dari %d Hurup dan %d Kata ", a, b); 
+}
