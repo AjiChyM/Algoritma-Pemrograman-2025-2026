500ab19b931d46200faaf778b670180a488f238f
AjiChyM <ajicahyamustaqim01@gmail.com>
2026-01-31 22:36:38 +0700

    latihan

---
+++ php/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+// membuat fungsi
+function hai(){
+echo "Assalammualaikum,\n ";
+echo "\nPerkenalkan, nama saya Aji\n";
+echo "Senang berkenalan dengan anda\n";
+}
+// memanggil fungsi yang sudah dibuat
+hai();
+hai();
+?>
---
+++ php/switchcase.php
@@ -0,0 +1,57 @@
+<?php
+$nama="Aji";
+$nim=(int)readline("Masukan NIM Anda Untuk mengecek saldo:");
+
+$saldo=$nim;
+
+echo "Halo, $nama!\n";
+echo "Silhakan pilih menu berikut:\n";
+$pilihan = (int)readline
+("=====Menu=====
+1. Cek Saldo
+2. Tarik Tunai
+3. Setor Tunai
+4. Tranfer
+5. keluar
+Pilih menu (1-5):\n");
+
+switch ($pilihan) {
+    case 1:
+        echo $nama . ",\n";
+        echo "Saldo Anda adalah: Rp. " . number_format($saldo, 0, ',', '.') . "\n";
+        break;
+    case 2:
+       $tarik = (int)readline("Masukkan jumlah tarik tunai: Rp. ");
+        if ($tarik > $saldo) {
+            echo "Saldo tidak mencukupi untuk tarik tunai sebesar Rp. " . number_format($tarik, 0, ',', '.') . "\n";
+        } else {
+            $saldo -= $tarik;
+            echo "Tarik tunai sebesar Rp. " . number_format($tarik, 0, ',', '.') . " berhasil.\n";
+            echo "Sisa saldo Anda: Rp. " . number_format($saldo, 0, ',', '.') . "\n";
+        }
+        break;
+    case 3:
+        $setor = (int)readline("Masukkan jumlah setor tunai: Rp. ");
+        $saldo += $setor;
+        echo "Setor tunai sebesar Rp. " . number_format($setor, 0, ',', '.') . " berhasil.\n";
+        echo "Saldo Anda sekarang: Rp. " . number_format($saldo, 0, ',', '.') . "\n";
+        break;
+    case 4:
+        $tranfer = (int)readline("Masukkan jumlah tranfer: Rp. ");
+        $saldo -= $tranfer;
+        echo "Tranfer sebesar Rp. " . number_format($tranfer, 0, ',', '.') . " berhasil.\n";
+        echo "Saldo Anda sekarang: Rp. " . number_format($saldo, 0, ',', '.') . "\n";
+        break;
+    case 5:
+         echo "Keluar dari menu. Sampai jumpa lagi!\n";
+        break;
+    default:
+        echo "Pilihan tidak valid. Silakan pilih menu antara 1-5.\n";
+        break;
+}
+
+
+
+echo "\nTerima kasih telah menggunakan layanan kami!\n";
+echo"==============";
+?>
