f3ac0da5d3dd8e9dddf17dc9fe98ae107b7fbe25
AjiChyM <ajicahyamustaqim01@gmail.com>
2026-01-31 20:23:21 +0700

    Update nim_array.cpp

--- c atau c++/nim_array.cpp
+++ c atau c++/nim_array.cpp
@@ -3,50 +3,41 @@
 using namespace std;
 
 int main() {
-    string nim;
-    cout << "Masukkan NIM: ";
-    cin >> nim;
+    string nim = "2510161";
+    vector<int> digits;
 
-    vector<int> digit(nim.length());
-    for (int i = 0; i < nim.length(); i++) {
-        digit[i] = nim[i] - '0';
+    // Memisahkan setiap digit dengan loop
+    for(char c : nim) {
+        digits.push_back(c - '0');
     }
 
-    int total = 0;
-    for (int d : digit) {
+    int total = 0, maks = digits[0], minim = digits[0];
+
+    for(int d : digits) {
         total += d;
+        if(d > maks) maks = d;
+        if(d < minim) minim = d;
     }
 
-    int maks = digit[0];
-    int minim = digit[0];
-    for (int d : digit) {
-        if (d > maks) maks = d;
-        if (d < minim) minim = d;
+    double rata = (double)total / digits.size();
+
+    // Reverse array
+    vector<int> rev;
+    for(int i = digits.size() - 1; i >= 0; i--) {
+        rev.push_back(digits[i]);
     }
 
-    double rata = (double) total / digit.size();
+    // Output
+    cout << "Array digit = [ ";
+    for(int d : digits) cout << d << " ";
+    cout << "]\n";
 
-    vector<int> rev(digit.size());
-    for (int i = 0; i < digit.size(); i++) {
-        rev[i] = digit[digit.size() - 1 - i];
-    }
+    cout << "Total digit = " << total << endl;
+    cout << "Digit maksimum = " << maks << endl;
+    cout << "Digit minimum = " << minim << endl;
+    cout << "Rata-rata digit = " << rata << endl;
 
-    cout << "Digit        : ";
-    for (int d : digit) {
-        cout << d << " ";
-    }
-    cout << endl;
-
-    cout << "Total        : " << total << endl;
-    cout << "Maks         : " << maks << endl;
-    cout << "Minim        : " << minim << endl;
-    cout << "Rata-rata    : " << rata << endl;
-
-    cout << "Reverse      : ";
-    for (int r : rev) {
-        cout << r << " ";
-    }
-    cout << endl;
-
-    return 0;
+    cout << "Reverse array = [ ";
+    for(int d : rev) cout << d << " ";
+    cout << "]\n";
 }
